bafa2d62716dddc5e4e731e49d616b14c3a96ec3
fedeargfyron <fedemgs15@gmail.com>
2022-11-16 19:14:23 -0300

    Mensajes de error personalizados

--- src/Components/SharedComponents/InputContainer/index.jsx
+++ src/Components/SharedComponents/InputContainer/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from './inputContainer.module.css'
 const InputContainer = ({children, label, errors}) => {
-  return (
-    <div className={styles.inputContainer}>
-        <label className={styles.inputLabel}>{label}</label>
-        {children}
-        {errors && <p className={styles.errorLabel}>Errores!</p>}
-    </div>
-  )
+    return (
+        <div className={styles.inputContainer}>
+            <label className={styles.inputLabel}>{label}</label>
+            {children}
+            {errors && 
+            <p className={styles.errorLabel}>
+                { errors.type === 'required' ? 'Required'
+                : errors.type === 'maxLength' ? 'Exceeded maximumn length'
+                : errors.type === 'minLength' ? 'Invalid minimum length'
+                : errors.type === 'min' ? 'Invalid quantity'
+                : 'Invalid characters'}
+            </p>}
+        </div>
+    )
 }
 
 export default InputContainer
